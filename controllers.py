318f0f08778ff758307dc0463d77c0c79b50a325
obryann <micacc@outlook.com>
2020-11-28 18:53:05 -0300

    Added index and added controllers.py

--- controllers.py
+++ controllers.py
@@ -1,1 +1,10 @@
 #Aqui vão as rotas e seus renders
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+    
